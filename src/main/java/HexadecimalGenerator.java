760349793cbf38b21d1bf8a58a03f64934fa7946
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-01 21:17:26 -0500

    Function: Created function to generate 32 random hexadecimal characters

---
+++ src/main/java/HexadecimalGenerator.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class HexadecimalGenerator {
+	
+	public static String generateHexadecimal() {
+        Random random = new Random();
+        StringBuilder hexBuilder = new StringBuilder();
+
+        // Generate 32 random hexadecimal characters
+        for (int i = 0; i < 32; i++) {
+            hexBuilder.append(Integer.toHexString(random.nextInt(16)));
+        }
+
+        // Format into UUID-like pattern
+        return hexBuilder.substring(0, 8) + "-" +
+               hexBuilder.substring(8, 12) + "-" +
+               hexBuilder.substring(12, 16) + "-" +
+               hexBuilder.substring(16, 20) + "-" +
+               hexBuilder.substring(20);
+    }
+}//end of class 
