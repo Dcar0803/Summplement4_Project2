5960806ba832b2f62ee9837325e6997a515e91c0
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-01 21:25:09 -0500

    Test: Created atesst function to hash a string

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -11,5 +11,14 @@ class Testing {
         assertTrue(hex.matches("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$"),
                 "Generated string should match the required hexadecimal format");
     }
+	
+	
+	 @Test
+	    void testHashString() {
+	        String input = "testInput";
+	        String hash = HexadecimalGenerator.hashString(input);
+	        assertNotNull(hash, "Hashed string should not be null");
+	        assertTrue(hash.matches("^[a-fA-F0-9]+$"), "Hashed string should only contain hexadecimal characters");
+	    }
 
 }//end of class 
