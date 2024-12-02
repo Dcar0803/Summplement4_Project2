0fe77325fc6bef64338bca0f231a89596116f390
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-01 21:42:35 -0500

    Test: Created test function for the class that holds an array field of strings

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -20,5 +20,15 @@ class Testing {
 	        assertNotNull(hash, "Hashed string should not be null");
 	        assertTrue(hash.matches("^[a-fA-F0-9]+$"), "Hashed string should only contain hexadecimal characters");
 	    }
+	 
+	 
+	 
+	 @Test
+	    void testStringArrayInsert() {
+	        StringArrayHandler arrayHandler = new StringArrayHandler(10);
+	        String hexString = "12345678-1234-1234-1234-1234567890ab";
+	        arrayHandler.insert(hexString, 12);
+	        assertEquals(hexString, arrayHandler.get(2), "The string should be inserted at index 2 (12 % 10 = 2)");
+	    }
 
 }//end of class 
